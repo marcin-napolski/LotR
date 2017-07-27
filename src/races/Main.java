f4329bb2db7fb4637cbb8f3e5bb8516c75aa71ca
marcin-napolski <marcin.napolski@gmail.com>
2017-07-27 12:17:18 +0200

    Create Main.java
    Adding Main.java

---
+++ src/races/Main.java
@@ -0,0 +1,14 @@
+package races;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// this part is just to demonstrate how the constructors are called by object initialization
+		Man aragorn = new Man();
+		Elf legolas = new Elf();
+		Dwarf gimli = new Dwarf();
+		
+
+	}
+
+}
