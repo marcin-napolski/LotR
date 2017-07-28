4bf35ad4b006596b6bd53bfd5d94135437f0f7b9
marcin-napolski <marcin.napolski@gmail.com>
2017-07-28 12:25:32 +0200

    Add files via upload

---
+++ src/races/Orc.java
@@ -0,0 +1,11 @@
+package races;
+
+public class Orc {
+
+	public Orc() {
+		System.out.println("I will crush you!");
+	}
+	public void fight() {
+		System.out.println("Taste my orcish sword!");		
+	}
+}
